144460b4f21f4bbb4912b382db66adaa7560d825
bkindtoevery1 <chayhyeon@gmail.com>
2025-08-20 15:52:32 +0900

    feat(lambda): Create lambda automative file downloader
    Downloads NYC TLC parquet data and stores it into S3

---
+++ fastapi-server/lambda_file_downloader.py
@@ -0,0 +1,52 @@
+import subprocess
+import json
+
+import requests
+import boto3
+from boto3.s3.transfer import S3UploadFailedError
+
+
+# send the failover message to slack
+def send_slack_message(webhook_url, message):
+    headers = {'Content-Type': 'application/json'}
+    payload = {'text': message}
+    response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
+    return response.status_code == 200
+
+
+s3 = boto3.client('s3')
+slack_webhook_url = "your-webhook-url"
+
+
+def lambda_handler(event, context):
+    url = "your-url"
+    # can use filename as the parameter with invoking lambda function with header(payload)
+    # filename = event['filename']
+    filename = "yellow_tripdata_2025-06.parquet"
+    local_filename = "/tmp/" + filename
+    bucket_name = "your-bucket-name"
+    key_name = f"nyc_taxi/{filename}"
+
+    try:
+        subprocess.run(["curl", "-o", local_filename, url + filename], check=True)
+    except subprocess.CalledProcessError as e:
+        send_slack_message(slack_webhook_url, f"Error downloading file: {e}")
+        return {
+            'statusCode': 500,
+            'body': f"Error downloading file: {e}"
+        }
+
+    try:
+        s3.upload_file(local_filename, bucket_name, key_name)
+    except S3UploadFailedError as e:
+        send_slack_message(slack_webhook_url, f"Error uploading file to S3: {e}")
+        return {
+            'statusCode': 500,
+            'body': f"Error uploading file to S3: {e}"
+        }
+
+    send_slack_message(slack_webhook_url, f"Successfully uploaded file: {filename}")
+    return {
+        'statusCode': 200,
+        'body': f"File {filename} uploaded to s3://{bucket_name}/{key_name}"
+    }
